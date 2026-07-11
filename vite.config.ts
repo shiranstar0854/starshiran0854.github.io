c8c781a61054d1bfff5369381da046e08f5d6a35
shiranstar0854 <shiranstar0854@users.noreply.github.com>
2026-07-11 20:03:33 +0800

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/starshiran0854.github.io/",
+  base: "/economy-game/",
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        index: "app.html",
+        index: "index.html",
       },
     },
   },
